ca53d1f728cfa6d89b2cb0b1e13be5b03968dd90
Ming <github@programmingbasics.org>
2014-07-29 19:26:32 -0400

    Documenting the list of things I should probably look at later in the form of some test stubs.

---
+++ jinq-jpa/test/org/jinq/jpa/JinqJPATodoTest.java
@@ -0,0 +1,43 @@
+package org.jinq.jpa;
+
+import org.junit.Test;
+
+/**
+ * Lists of tests for things that we eventually plan on implementing 
+ *
+ */
+public class JinqJPATodoTest 
+{
+   @Test
+   public void testSortSkipLimit() {}
+
+   @Test
+   public void testNumericPromotionCasts() {}
+   
+   @Test
+   public void testMultiAggregate() {}
+
+   @Test
+   public void testGrouping() {}
+   
+   @Test
+   public void testSubqueries() {}
+   
+   @Test
+   public void testUnaryExpressions() {}
+
+   @Test
+   public void testFunctionsLikeEtc() {}
+   
+   @Test
+   public void testNull() {}
+   
+   @Test
+   public void testInnerJoins() {}
+   
+   @Test
+   public void testSelectCase() {}
+   
+   @Test
+   public void testPathExplosion() {}
+}
