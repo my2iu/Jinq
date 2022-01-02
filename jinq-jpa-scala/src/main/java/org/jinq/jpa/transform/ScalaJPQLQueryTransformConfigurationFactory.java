1fcfb33d0ee5b41b094990b8254fc58d6ba59972
Ming <git@user00.com>
2022-01-02 14:20:58 -0500

    Removing Scala support since it's so out-dated

--- jinq-jpa-scala/src/main/java/org/jinq/jpa/transform/ScalaJPQLQueryTransformConfigurationFactory.java
+++
@@ -1,9 +0,0 @@
-package org.jinq.jpa.transform;
-
-public class ScalaJPQLQueryTransformConfigurationFactory extends JPQLQueryTransformConfigurationFactory
-{
-   public JPQLQueryTransformConfiguration createConfig()
-   {
-      return new ScalaJPQLQueryTransformConfiguration();
-   }
-}
